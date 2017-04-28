8000c85529a8274d3af068f4428bf833615d13e0
Alexandra Holcombe <alexandra.holcombe@gmail.com>
2017-04-28 09:37:13 -0700

    Add ApplicationUser model, SOCContext model

---
+++ src/SonOfCod/Models/SOCContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SonOfCod.Models;
+
+namespace SonOfCod.Models
+{
+    public class SOCContext : IdentityDbContext<ApplicationUser>
+    {
+        public SOCContext()
+        {
+        }
+
+        public SOCContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=SonOfCod;integrated security=True");
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
