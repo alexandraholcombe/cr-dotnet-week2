af9e32c5eccb49f3c75d2ba19cd070aacea7c25a
Alexandra Holcombe <alexandra.holcombe@gmail.com>
2017-04-28 12:03:07 -0700

    Create subscriber class

---
+++ src/SonOfCod/Models/Subscriber.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SonOfCod.Models
+{
+    public class Subscriber
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        public string Location { get; set; }
+
+        public string Reason { get; set; }
+    }
+}
