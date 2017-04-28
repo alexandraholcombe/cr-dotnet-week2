f4ca28d7dc21aedba678a79e28b6c2602dbb24ad
Alexandra Holcombe <alexandra.holcombe@gmail.com>
2017-04-28 10:23:57 -0700

    Create admin login page

--- src/SonOfCod/Controllers/AdminController.cs
+++ src/SonOfCod/Controllers/AdminController.cs
@@ -1,16 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
 using SonOfCod.Models;
+using SonOfCod.ViewModels;
+using System.Threading.Tasks;
 
 namespace SonOfCod.Controllers
 {
-    public class AccountController : Controller
+    public class AdminController : Controller
     {
         private readonly SOCContext _db;
         private readonly UserManager<Admin> _userManager;
         private readonly SignInManager<Admin> _signInManager;
 
-        public AccountController(UserManager<Admin> userManager, SignInManager<Admin> signInManager, SOCContext db)
+        public AdminController(UserManager<Admin> userManager, SignInManager<Admin> signInManager, SOCContext db)
         {
             _userManager = userManager;
             _signInManager = signInManager;
@@ -20,6 +22,25 @@ namespace SonOfCod.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(model.UserName, model.Password, isPersistent: true, lockoutOnFailure: false);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
         }
     }
 }
