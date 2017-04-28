9cad8f44361b25acabdb874dfccf69cb7592c1e3
Alexandra Holcombe <alexandra.holcombe@gmail.com>
2017-04-28 16:44:08 -0700

    Create admin link in navbar

--- src/SonOfCod/Controllers/HomeController.cs
+++ src/SonOfCod/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SonOfCod.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace SonOfCod.Controllers
 {
@@ -17,6 +18,20 @@ namespace SonOfCod.Controllers
             ViewBag.Thing = "thing";
             ViewBag.FunBlurb = funblurb;
             return View();
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var thisContent = _db.Content.FirstOrDefault(c => c.Id == id);
+            return View(thisContent);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Content content)
+        {
+            _db.Entry(content).State = EntityState.Modified;
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
